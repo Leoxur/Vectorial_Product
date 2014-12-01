8f9c32d3f071508ee49216313955b8bc49e8afe9
Leoxur <Leoxur@users.noreply.github.com>
2014-12-01 22:28:01 +0100

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,15 @@
+#ifndef PRODOTTO_H
+#define PRODOTTO_H
+#define SIZE 10
+/* Librerie */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Prototipi */
+int conversione (int termine[], char *string);
+int prodotto (int termine1[], int termine2[]);
+
+
+
+#endif

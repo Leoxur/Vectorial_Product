04823d46482f44d2d564d756f3a0136f30c1e4bd
Leoxur <Leoxur@users.noreply.github.com>
2014-12-03 22:51:58 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 #include <string.h>
 
 int main (int argc, char *argv[SIZE]){
-	
-	int i, termine1[SIZE], termine2[SIZE];
-	
-	printf("argv1=%s\n", *(argv+1) );	
-	printf("argv1=%s\n", *(argv+2) );
 
-	conversione (termine1, *(argv+1) );
-	conversione (termine2, *(argv+2) );
-	
+	int i, termine1[SIZE], termine2[SIZE];
+	printf ("\n\e[1;42m------QUESTI SONO GLI ARGOMENTI PASSATI------\e[00m\n");
+	printf("\targv1 = %s\n", *(argv+1) );
+	printf("\targv2 = %s\n\n", *(argv+2) );
+
+	parser (termine1, *(argv+1) );
+	parser (termine2, *(argv+2) );
+
 	return prodotto (termine1, termine2);
 }

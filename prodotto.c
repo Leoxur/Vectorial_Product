81d5ba2eac2ec6215685dbf927f34cd29dddcddc
Leoxur <Leoxur@users.noreply.github.com>
2014-12-03 22:52:16 +0100

    Update prodotto.c

--- prodotto.c
+++ prodotto.c
@@ -8,13 +8,13 @@ int prodotto (int termine1[], int termine2[]){
 	int i, prodotto[SIZE], risultato = 0;
 
 	extern int flag;
-
+	printf("\t\e[1;36m...Prodotto in corso...\e[00m\n");
 	for (i=0; i<flag; i++){
 		*(prodotto+i) = *(termine1+i)* *(termine2+i);
 		risultato += prodotto[i];
 
-		printf("Prodotto in corso[%d]= %d\n", i, prodotto[i]);
-		}
-	printf("Il prodotto vettoriale e': %d\n", risultato);
+		printf("P[%d]= %d\n", i+1, prodotto[i]);
+		}	//1;40 grassetto
+	printf("\e[1;40mIl prodotto vettoriale e': %d\e[00m\n", risultato);
 	return risultato;
 }

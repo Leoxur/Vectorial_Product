bd15238cff6af6d3bf36fac51cb6c30c0324dd2d
Leoxur <Leoxur@users.noreply.github.com>
2014-12-03 22:51:15 +0100

    Update intero.c

--- intero.c
+++ intero.c
@@ -1,24 +1,25 @@
-#include "lib.h"
+#include "lib.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 int flag;
-int conversione ( int termine[], char *string){
+int parser ( int termine[], char *string){
 
 	int i; //Posizione
 	char *s; //vettore di caratteri
 
 	s = strtok (string, " " "," "{" "}"); //Termini che la funzione deve bypassare
-	
+
 	i=0;
 	while ( s != NULL ){
 		//Atoi è una funzione che converte stringhe in interi
-		termine[i] = atoi (s);	
+		termine[i] = atoi (s);
 
-		printf("s[%d]=%s\t", i, s);
-		printf("termine[%d]=%d\n", i, termine[i]);
+		printf("termine[%d]=%d\n", i+1, termine[i]);
 
 		s = strtok (NULL, " " "," "{" "}");
+		if( i==9)
+			printf("\n");
 		i++;
 	}
 
